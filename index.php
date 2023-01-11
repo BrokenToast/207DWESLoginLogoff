ad2ec32a5965ba480f3fe02e7ffdd96a684977d1
KingToast <93706315+BrokenToast@users.noreply.github.com>
2023-01-11 09:55:44 +0100

    11012023 Empezamos LoginLogoff

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <header>
+        <h1>LoginLogoff</h1>
+    </header>
+    <nav>
+        <ul>
+            
+        </ul>
+    </nav>
+    <main>
+        
+    </main>
+    <footer>
+        <p>Luis Pérez Astorga® 2022-2023</p>
+    </footer>
+</body>
+</html>
